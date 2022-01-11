5852937e206dd83a5f227a505e523e5146659c4d
gagan_gayari <gagan.gayari@gmail.com>
2022-01-11 17:45:43 +0530

    Added style.css and test.js

--- demo.c
+++
@@ -1,9 +0,0 @@
-
-#include<stdio.h>
-
-int main(){
-  printf("Hello World ! \n");
-  printf("Hellodude\n");
-  printf("hey");
-  return 0;
-}
---
+++ test.js
@@ -0,0 +1,9 @@
+console.log(document.domain);
+
+function submit(){
+    // var submit = document.getElementById('button1');
+    window.alert("Submitted");
+}
+
+// alert(1);
+
