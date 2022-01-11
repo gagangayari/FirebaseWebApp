e0bf04631642a94b2083832945b406a96948b9b1
gagan_gayari <gagan.gayari@gmail.com>
2022-01-11 18:10:39 +0530

    Added console.log(doc.forms)

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 console.log(document.domain);
-
+console.log(document.forms);
 function submit(){
     // var submit = document.getElementById('button1');
     window.alert("Submitted");
+    
 }
 
 // alert(1);
